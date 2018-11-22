348c320cef3b368ca9b16d110a8c4e49cca3fb81
xiexiang <xiexiang@sunniwell.net>
2018-11-22 22:33:41 +0800

    MODIFY:增加wifi回调函数

--- user/sw_wifi.c
+++ user/sw_wifi.c
@@ -5,7 +5,41 @@
 #define AP_SSID "sunniwell_gt"
 #define AP_PASSWD "sunniwell.1201"
 
-static void staton_mode(void)
+static void wifi_handle_event_cb(System_Event_t *evt)
+{
+	switch(evt->event_id)
+	{
+		case EVENT_STAMODE_CONNECTED:
+			SW_LOG_DEBUG("connect to ssid%s,channel	%d\n",evt->event_info.connected.ssid,
+					evt->event_info.connected.channel);
+			break;
+		case EVENT_STAMODE_DISCONNECTED:
+			SW_LOG_INFO("disconnect from ssid %s,reason %d\n",evt->event_info.disconnected.ssid,
+					evt->event_info.disconnected.reason);
+			break;
+		case EVENT_STAMODE_AUTHMODE_CHANGE:
+			SW_LOG_INFO("mode:%d->%d\n",evt->event_info.auth_change.old_mode,
+					evt->event_info.auth_change.new_mode);
+			break;
+		case EVENT_STAMODE_GOT_IP:
+			SW_LOG_INFO("ip:"IPSTR",mask:"IPSTR",gw:"IPSTR"\n",IP2STR(&evt->event_info.got_ip.ip),
+					IP2STR(&evt->event_info.got_ip.mask),IP2STR(&evt->event_info.got_ip.gw));
+			break;
+		case EVENT_SOFTAPMODE_STACONNECTED:
+			SW_LOG_INFO("station:"MACSTR"join,AID=%d\n",MAC2STR(evt->event_info.sta_connected.mac),
+					evt->event_info.sta_connected.aid);
+			break;
+		case EVENT_SOFTAPMODE_STADISCONNECTED:
+			SW_LOG_INFO("station:"MACSTR"leave,AID=%d\n",MAC2STR(evt->event_info.sta_disconnected.mac),
+					evt->event_info.sta_disconnected.aid);
+			break;
+		default:
+			break;
+	}
+}
+
+
+static void station_mode(void)
 {
 	wifi_set_opmode(STATIONAP_MODE);
 	struct station_config *config=(struct station_config*)zalloc(sizeof(struct	station_config));
@@ -25,10 +59,9 @@ static void staton_mode(void)
 
 int sw_wifi_init(int mode)
 {
-	void *work_func = NULL;
 	if(mode == STATION_MODE)
-		work_func = staton_mode;
-	staton_mode();
+		station_mode();
 	//xTaskCreate(work_func,"wifi_config",    256,    NULL,   2,  NULL);
+	wifi_set_event_handler_cb(wifi_handle_event_cb);
 	return 0;
 }
--- user/sw_wifi.h
+++ user/sw_wifi.h
@@ -2,6 +2,7 @@
 #define __SW_WIFI_H__
 
 #include "esp_sta.h"
+typedef void (*wifi_conected_cb_t)(void);
 int sw_wifi_init(int mode);
 
 #endif
